92bc4c89b41a97c30a4d2ce3b95643bdb55a03ad
mgualtier <matteo.gualtieri95@gmail.com>
2025-03-03 13:27:21 +0100

    added app properties

---
+++ ProdottoBE/src/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO prodotto (nome, categoria, prezzo, immagine) VALUES
+('Coca Cola', 'bevande', 2.50, 'images/coca.jpg'),
+('Fanta', 'bevande', 2.30, 'images/fanta.jpg'),
+('Hamburger', 'cibo', 5.99, 'images/hamburger.jpg'),
+('Patatine', 'cibo', 3.50, 'images/patatine.jpg'),
+('Cheeseburger', 'cibo', 6.50, 'images/cheeseburger.jpg');
